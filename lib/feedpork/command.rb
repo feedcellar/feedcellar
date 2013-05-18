4c1db79a8613868676d5c17152da0c314744a5d0
Masafumi Yokoyama <myokoym@gmail.com>
2013-05-18 18:58:19 +0900

    Move working directory to "~/.feedpork"

--- lib/feedpork/command.rb
+++ lib/feedpork/command.rb
@@ -12,31 +12,35 @@ module Feedpork
 
     desc "init", "Initialize a config file."
     def init
-      if File.exist?(SAVE_FILE)
-        puts "Already exist. See #{SAVE_FILE}"
+      unless Dir.exist?(work_dir)
+        Dir.mkdir(work_dir)
+      end
+
+      if File.exist?(save_file)
+        puts "Already exist. See #{save_file}"
         exit(false)
       end
 
       feeds = {}
-      File.open(SAVE_FILE, "w") do |f|
+      File.open(save_file, "w") do |f|
         YAML.dump(feeds, f)
       end
     end
 
     desc "add URL", "Add a feed url to config file."
     def add(feed_url)
-      feeds = YAML.load_file(SAVE_FILE)
+      feeds = YAML.load_file(save_file)
 
       feeds[feed_url] = {}
 
-      File.open(SAVE_FILE, "w") do |f|
+      File.open(save_file, "w") do |f|
         YAML.dump(feeds, f)
       end
     end
 
     desc "read", "Read a feed titles."
     def read
-      feeds = YAML.load_file(SAVE_FILE)
+      feeds = YAML.load_file(save_file)
 
       feeds.keys.each do |feed_url|
         begin
@@ -54,6 +58,15 @@ module Feedpork
           puts
         end
       end
+    end
+
+    private
+    def save_file
+      File.join(work_dir, SAVE_FILE)
+    end
+
+    def work_dir
+      work_dir = File.join(File.expand_path("~"), ".feedpork")
     end
   end
 end
