09a56b189551465c196227627b23b9020eced5dc
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-08 16:44:37 +0900

    Extract parse of URL for resource of feed

---
+++ lib/feedcellar/resource.rb
@@ -0,0 +1,36 @@
+require "rss"
+
+module Feedcellar
+  class Resource
+    def self.parse(url)
+      begin
+        rss = RSS::Parser.parse(url)
+      rescue RSS::InvalidRSSError
+        rss = RSS::Parser.parse(url, false)
+      rescue
+        $stderr.puts "WARNNING: #{$!} (#{url})"
+        return nil
+      end
+
+      unless rss
+        $stderr.puts "ERROR: Invalid URL"
+        return nil
+      end
+
+      resource = {}
+      if rss.is_a?(RSS::Atom::Feed)
+        resource["xmlUrl"] = url
+        resource["title"] = rss.title.content
+        resource["htmlUrl"] = rss.link.href
+        resource["description"] = rss.dc_description
+      else
+        resource["xmlUrl"] = url
+        resource["title"] = rss.channel.title
+        resource["htmlUrl"] = rss.channel.link
+        resource["description"] = rss.channel.description
+      end
+
+      resource
+    end
+  end
+end
