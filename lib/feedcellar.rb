d13d7eeba74b21eb184d70e466c119a0a59fe62e
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-09 09:04:21 +0900

    Add missing require

--- lib/feedcellar.rb
+++ lib/feedcellar.rb
@@ -2,6 +2,8 @@ require "feedcellar/version"
 require "feedcellar/groonga_database"
 require "feedcellar/opml"
 require "feedcellar/command"
+require "feedcellar/resource"
+require "feedcellar/feed"
 
 module Feedcellar
 end
