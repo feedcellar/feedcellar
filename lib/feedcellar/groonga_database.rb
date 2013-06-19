76f4aba75c7371281a224eb336ee2c1997d5c420
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-19 23:16:33 +0900

    Support for migration to 0.3.0 for existing database

--- lib/feedcellar/groonga_database.rb
+++ lib/feedcellar/groonga_database.rb
@@ -11,7 +11,12 @@ module Feedcellar
       path = File.join(base_path, "feedcellar.db")
       if File.exist?(path)
         @database = Groonga::Database.open(path)
+        begin
         populate_schema
+        rescue Groonga::Schema::ColumnCreationWithDifferentOptions
+          # NOTE: migrate to feedcellar-0.3.0 from 0.2.2 or earlier.
+          populate_new_schema
+        end
       else
         FileUtils.mkdir_p(base_path)
         populate(path)
@@ -106,6 +111,82 @@ module Feedcellar
                             :default_tokenizer => "TokenBigram") do |table|
           table.index("Feeds.title")
           table.index("Feeds.description")
+        end
+      end
+    end
+
+    def populate_new_schema
+      populate_old_schema
+      add_new_column
+      migrate_value
+      delete_old_column
+    end
+
+    def populate_old_schema
+      Groonga::Schema.define do |schema|
+        schema.create_table("Resources", :type => :hash) do |table|
+          table.text("text")
+          table.short_text("isComment")
+          table.short_text("isBreakpoint")
+          table.short_text("created")
+          table.short_text("category")
+          table.text("description")
+          table.short_text("url")
+          table.short_text("htmlUrl")
+          table.short_text("xmlUrl")
+          table.short_text("title")
+          table.short_text("version")
+          table.short_text("language")
+        end
+
+        schema.create_table("Feeds", :type => :hash) do |table|
+          table.short_text("resource")
+          table.short_text("title")
+          table.short_text("link")
+          table.text("description")
+          table.time("date")
+        end
+
+        schema.create_table("Terms",
+                            :type => :patricia_trie,
+                            :key_normalize => true,
+                            :default_tokenizer => "TokenBigram") do |table|
+          table.index("Feeds.title")
+          table.index("Feeds.description")
+        end
+      end
+    end
+
+    def add_new_column
+      Groonga::Schema.define do |schema|
+        schema.change_table("Feeds") do |table|
+          table.reference("new_resource", "Resources")
+        end
+      end
+    end
+
+    def migrate_value
+      feeds = Groonga["Feeds"]
+      resources = Groonga["Resources"]
+      tmp_resources = {}
+      resources.each do |resource|
+        tmp_resources[resource.xmlUrl] = resource.title
+      end
+
+      feeds.each do |feed|
+        feed["new_resource"] = resources[tmp_resources[feed.resource]]
+        feed["resource"] = nil
+      end
+    end
+
+    def delete_old_column
+      Groonga::Schema.define do |schema|
+        schema.change_table("Feeds") do |table|
+          table.remove_column("resource")
+        end
+
+        schema.change_table("Feeds") do |table|
+          table.rename_column("new_resource", "resource")
         end
       end
     end
