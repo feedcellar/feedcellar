e4ca25601f010bacae3621708cf41612edbc5036
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-24 22:52:09 +0900

    test: use startup-shutdown

--- test/test-command.rb
+++ test/test-command.rb
@@ -5,11 +5,11 @@ require "feedcellar/command"
 require "feedcellar/groonga_database"
 
 class CommandTest < Test::Unit::TestCase
-  def setup
-    @tmpdir = File.join(File.dirname(__FILE__), "tmp", "database")
-    FileUtils.mkdir_p(@tmpdir)
-    @command = Feedcellar::Command.new
-    @command.instance_variable_set(:@database_dir, @tmpdir)
+  def self.startup
+    @@tmpdir = File.join(File.dirname(__FILE__), "tmp", "database")
+    FileUtils.mkdir_p(@@tmpdir)
+    @@command = Feedcellar::Command.new
+    @@command.instance_variable_set(:@database_dir, @@tmpdir)
   end
 
   def test_command
@@ -17,7 +17,7 @@ class CommandTest < Test::Unit::TestCase
     s = ""
     io = StringIO.new(s)
     $stdout = io
-    @command.version
+    @@command.version
     assert_equal("#{Feedcellar::VERSION}\n", s)
     $stdout = STDOUT
 
@@ -25,22 +25,22 @@ class CommandTest < Test::Unit::TestCase
     s = ""
     io = StringIO.new(s)
     $stderr = io
-    assert_equal(1, @command.register("hoge"))
+    assert_equal(1, @@command.register("hoge"))
     assert_equal("ERROR: Invalid URL\n", s)
     $stderr = STDERR
 
     # confirm register command
-    @command.register("http://myokoym.github.io/entries.rss")
-    @command.register("https://rubygems.org/gems/mister_fairy/versions.atom")
-    Feedcellar::GroongaDatabase.new.open(@tmpdir) do |database|
+    @@command.register("http://myokoym.github.io/entries.rss")
+    @@command.register("https://rubygems.org/gems/mister_fairy/versions.atom")
+    Feedcellar::GroongaDatabase.new.open(@@tmpdir) do |database|
       assert_equal(2, database.resources.size)
     end
 
     # confirm import command
     file = File.join(File.dirname(__FILE__), "fixtures", "subscriptions.xml")
-    @command.import(file)
-    @command.collect
-    Feedcellar::GroongaDatabase.new.open(@tmpdir) do |database|
+    @@command.import(file)
+    @@command.collect
+    Feedcellar::GroongaDatabase.new.open(@@tmpdir) do |database|
       # NOTE: a tag of outline is not register.
       assert_equal(3, database.resources.size)
       assert_true(database.feeds.count > 0)
@@ -50,7 +50,7 @@ class CommandTest < Test::Unit::TestCase
     s = ""
     io = StringIO.new(s)
     $stdout = io
-    @command.export
+    @@command.export
     assert_equal(1, s.scan(/<opml/).size)
     assert_equal(3, s.scan(/<outline/).size)
     $stdout = STDOUT
@@ -59,17 +59,17 @@ class CommandTest < Test::Unit::TestCase
     s = ""
     io = StringIO.new(s)
     $stdout = io
-    @command.search("ruby")
+    @@command.search("ruby")
     assert_true(s.size > 100)
     $stdout = STDOUT
 
     # confirm unregister command
-    @command.unregister("my_letter")
-    Feedcellar::GroongaDatabase.new.open(@tmpdir) do |database|
+    @@command.unregister("my_letter")
+    Feedcellar::GroongaDatabase.new.open(@@tmpdir) do |database|
       assert_equal(2, database.resources.size)
     end
-    @command.unregister("https://rubygems.org/gems/mister_fairy/versions.atom")
-    Feedcellar::GroongaDatabase.new.open(@tmpdir) do |database|
+    @@command.unregister("https://rubygems.org/gems/mister_fairy/versions.atom")
+    Feedcellar::GroongaDatabase.new.open(@@tmpdir) do |database|
       assert_equal(1, database.resources.size)
     end
 
@@ -77,12 +77,12 @@ class CommandTest < Test::Unit::TestCase
     s = ""
     io = StringIO.new(s)
     $stdout = io
-    @command.latest
+    @@command.latest
     assert_true(s.size > 0)
     $stdout = STDOUT
   end
 
-  def teardown
-    FileUtils.rm_rf(@tmpdir)
+  def self.shutdown
+    FileUtils.rm_rf(@@tmpdir)
   end
 end
